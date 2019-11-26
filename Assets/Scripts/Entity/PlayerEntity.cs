a59ddabf7f7d6a9d5df07d08b87b3fd37d8d8310
DominicEiswirt <dominic.eiswirt@boombytegames.com>
2019-11-26 17:23:34 +0100

    First few experiments with entity object system. Managed to add objects to the playfab game manager, next up I will probably try to figure out how to read it from JSON, and then after that I will write the last used weapon to it.
    Alternatively I will first create a way to write the last played weapon, and THEN figure out how to read from JSON. I think the latter will save more time?

---
+++ Assets/Scripts/Entity/PlayerEntity.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public static class PlayerEntity
+{
+    public static string id;
+    public static string type;
+}
---
+++ Assets/Scripts/Entity/PlayfabEntity.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+using PlayFab.AuthenticationModels;
+using PlayFab.DataModels;
+public class PlayfabEntity : MonoBehaviour
+{
+    public bool getEntity;
+    void Start()
+    {
+        // if (!getEntity)
+        // {
+        //setEntity
+        PlayFabAuthenticationAPI.GetEntityToken(new GetEntityTokenRequest(), OnEntitySuccess, OnEntityFailure);
+        //  }
+        //  else
+        //  {
+
+        //   }
+    }
+
+    private void OnEntitySuccess(GetEntityTokenResponse responseResult)
+    {
+        PlayerEntity.id = responseResult.Entity.Id;
+        PlayerEntity.type = responseResult.Entity.Type;
+        SetTestEntityOnPlayer();
+    }
+
+    private void OnEntityFailure(PlayFabError error)
+    {
+        Debug.LogError(error);
+        Debug.LogError(error.ErrorMessage);
+        Debug.LogError(error.ErrorDetails);
+    }
+
+    private void SetTestEntityOnPlayer()
+    {
+        Dictionary<string, object> data = new Dictionary<string, object>()
+        {
+            { "Health", 100 },
+            { "Stamina", 100 },
+        };
+
+        List<SetObject> dataList = new List<SetObject>()
+        {
+            new SetObject()
+            {
+                ObjectName = "TestPlayerData",
+                DataObject = data
+            },
+        };
+
+        PlayFabDataAPI.SetObjects(new SetObjectsRequest()
+        {
+            Entity = new PlayFab.DataModels.EntityKey { Id = PlayerEntity.id, Type = PlayerEntity.type },
+            Objects = dataList,
+        }, (setResult) =>
+        {
+            Debug.Log(setResult.ProfileVersion);
+        },
+        (error) =>
+        {
+            Debug.Log(error.ErrorDetails);
+        });
+
+        Debug.Log("|||||||||||||||||||||||||||||||||||||||||||||||");
+        RequestEntityInfo();
+    }
+
+    private void RequestEntityInfo()
+    {
+        GetObjectsRequest request = new GetObjectsRequest
+        {
+            Entity = new PlayFab.DataModels.EntityKey
+            {
+                Id = PlayerEntity.id,
+                Type = PlayerEntity.type
+            }
+        };
+        PlayFabDataAPI.GetObjects(
+            request,
+            result =>
+            {
+                Debug.Log(result.Objects["TestPlayerData"].ToJson());
+            },
+            (error) => Debug.Log(error));
+            
+
+    }
+}
